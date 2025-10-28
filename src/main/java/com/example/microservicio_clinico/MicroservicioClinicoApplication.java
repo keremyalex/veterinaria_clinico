3e9459404c8d19a624bf45ca178d092f19abe032
alex keremy <keremy.alex@gmail.com>
2025-10-28 16:14:08 -0400

    first commit

---
+++ src/main/java/com/example/microservicio_clinico/MicroservicioClinicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_clinico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClinicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioClinicoApplication.class, args);
+    }
+
+}
